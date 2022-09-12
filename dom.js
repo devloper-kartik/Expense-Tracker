cfc3514137626faec0b4e76b5f1edc4dbf78beea
devloper-kartik <112373403+devloper-kartik@users.noreply.github.com>
2022-09-12 15:13:00 +0530

    server upload and download

--- dom.js
+++ dom.js
@@ -12,61 +12,63 @@ function afterSubmit(){
     let desp = document.getElementById("desp").value;
     let cho = document.getElementById("choose").value;
 
-    let obj = {
+    const obj = {
         amount : amt,
         description : desp,
         choose : cho ,
-    }
+    } 
 
-    localStorage.setItem(desp,JSON.stringify(obj));
+    axios.post("https://crudcrud.com/api/5511b798f3ae4184a7ab0de18381953c/Expence",obj)
+    .then(res=>console.log(res, "Succesfully uploded"))
+    .catch(err=>console.log(err));
 }
 
 
-function afterLoad(){
-    let total =document.getElementById("total");
-    let totalAmount = 0;
-    for(let i=0;i<localStorage.length;i++){
-        var obj = JSON.parse(localStorage.getItem(localStorage.key(i)));
-        let li = document.createElement("li");
+function afterLoad(e){
+    axios.get("https://crudcrud.com/api/5511b798f3ae4184a7ab0de18381953c/Expence")
+    .then(res=>{
+        let totalAmount = 0;
+        res.data.forEach((obj)=>{
+            let li = document.createElement("li");
+            let del = document.createElement("button");
+            let edit = document.createElement("button");
+            del.appendChild(document.createTextNode("Delete"));
+            del.className = "delete";
+            edit.appendChild(document.createTextNode("Edit"));
+            edit.className = "edit";
+            totalAmount += parseInt(obj.amount) ;
+            li.appendChild(document.createTextNode("$"+obj.amount + " "));
+            li.appendChild(document.createTextNode(obj.description));
+            li.appendChild(document.createTextNode(" "+obj.choose +" "));
+            li.appendChild(edit);
+            li.appendChild(del);
+            ul.appendChild(li);
+        });
+        total.appendChild(document.createTextNode("$"+totalAmount));
+    })
+    .catch(err=>console.log(err));
 
-        let del = document.createElement("button");
-        let edit = document.createElement("button");
-        del.appendChild(document.createTextNode("Delete"));
-        del.className = "delete";
-        edit.appendChild(document.createTextNode("Edit"));
-        edit.className = "edit";
-        console.log(obj.amount);
-        totalAmount += parseInt(obj.amount) ;
-
-        li.appendChild(document.createTextNode("$"+obj.amount + " "));
-        li.appendChild(document.createTextNode(obj.description));
-        li.appendChild(document.createTextNode(" "+obj.choose +" "));
-        li.appendChild(edit);
-        li.appendChild(del);
-        ul.appendChild(li);
-    }
-    total.appendChild(document.createTextNode("$"+totalAmount));
 }
 
 function listClick(e){
     let li = e.target.parentElement;
     let key=li.childNodes[1].textContent;
-    // console.log(JSON.parse(localStorage.getItem(key)));
-    if(e.target.classList.contains('delete')){
-        localStorage.removeItem(key);
-        ul.removeChild(li);
-    }
+    console.log(key);
+    // if(e.target.classList.contains('delete')){
+    //     localStorage.removeItem(key);
+    //     ul.removeChild(li);
+    // }
 
-    if(e.target.classList.contains('edit')){
+    // if(e.target.classList.contains('edit')){
         
-        let obj = JSON.parse(localStorage.getItem(key));
-        let amt = document.getElementById("amount");
-        let desp = document.getElementById("desp");
-        let cho = document.getElementById("choose");
-        amt.value = obj.amount;
-        desp.value = obj.description;
-        cho.value = obj.choose;
-        localStorage.removeItem(key);
-        ul.removeChild(li);
-    }
+    //     let obj = JSON.parse(localStorage.getItem(key));
+    //     let amt = document.getElementById("amount");
+    //     let desp = document.getElementById("desp");
+    //     let cho = document.getElementById("choose");
+    //     amt.value = obj.amount;
+    //     desp.value = obj.description;
+    //     cho.value = obj.choose;
+    //     localStorage.removeItem(key);
+    //     ul.removeChild(li);
+    // }
 }
